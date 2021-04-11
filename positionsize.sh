67fed15b3cf51026f0711f22ee5807312999cf9b
Wes Wright <inventor487@gmail.com>
2021-04-11 02:03:03 -0400

    add position size script rough draft

---
+++ positionsize.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while getopts ":ls" opt; do
+  case ${opt} in
+    l )   #long trade
+      read -p "Risk size" risk
+      read -p "Entry (negative): " entry
+      read -p "Stop (positive): " stoporder
+      ;;
+
+    s )  #short trade
+      read -p "Risk size" risk
+      read -p "Entry (positive): " entry
+      read -p "Stop (negative): " stoporder
+      ;;
+
+    \? )
+      echo "Invalid option: -$OPTARG" 1>&2
+      ;;
+  esac
+done
+echo "scale=2; $risk/($entry+$stoporder)" | bc
+
+
+
